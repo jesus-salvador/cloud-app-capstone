6aebabda31cb59b98f10b9f8679f6e406c04741d
Jesus Meza <jsmsmail@gmail.com>
2023-03-29 13:19:32 -0600

    CarMake and CarModel

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,26 +2,53 @@ from django.db import models
 from django.utils.timezone import now
 
 
-# Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=250, default='Car Maker')
+    description = models.CharField(max_length=1000)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return f'CarMake: {self.name}'
 
+class CardModel(models.Model):
+    card_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=250, default='Car Model')
+    dealer_id = models.IntegerField(null=False, default=1)
+    SEDAN = 'sedan'
+    SUV = 'suv'
+    WAGON = 'wagon'
+    PICKUP = 'pickup'
+    VANS = 'vans'
+    MINIVANS = 'minivans'
+    SPORT = 'sport'
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (PICKUP, 'Pickup'),
+        (VANS, 'Vans'),
+        (MINIVANS, 'Minivans'),
+        (SPORT, 'Sport'),
+    ]
+    type = models.CharField(
+        choices=TYPE_CHOICES,
+        default=SEDAN,
+        max_length=10,
+        null=False,
+    )
+    year = models.DateField(null=False, default=now)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return f'CardModel: {self.name}'
 
+class CarDealer:
+    '''CarDealer (python class) to hold dealer data'''
+    pass
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class DealerReview:
+    '''DealerReview (python class) to hold dealer data'''
+    pass
 
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
